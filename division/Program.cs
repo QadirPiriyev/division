4e017108b06eb00560b04881c6bcb5d00b8b3ff4
QadirPiriyev <qadirpiriyev@yahoo.com>
2022-12-11 01:36:27 +0400

    Add project files.

---
+++ division/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("zehmet olmasa 1 eded daxil edin");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("zehmet olmasa 1 eded daha daxil edin");
+                int b = Convert.ToInt32(Console.ReadLine());
+                int cavab = a % b;
+                Console.WriteLine("bolunmeden alinan qaliq : " + cavab);
+            }
+        }
+    }
+}
